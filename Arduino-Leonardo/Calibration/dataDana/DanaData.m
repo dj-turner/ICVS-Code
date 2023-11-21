133601f6adf86552626e23374244b8a8003adac4
OPL NiceBiscuit <dana.turner@psy.ox.ac.uk>
2023-11-21 16:58:25 +0000

    danadata

---
+++ Arduino-Leonardo/Calibration/dataDana/DanaData.m
@@ -0,0 +1,45 @@
+clear; close all; clc;
+
+tbl = readtable("DanaCheck.xlsx");
+
+bestTbl = tbl(strcmp(tbl.MatchType, "Best"), ~strcmp(tbl.Properties.VariableNames, "MatchType"));
+
+sessionNum = max(bestTbl.Session);
+
+summaryArray = NaN(sessionNum, width(bestTbl));
+
+for session = 1:sessionNum
+    sessionTbl = bestTbl(bestTbl.Session == session, :);
+    meanSessionTbl = mean(table2array(sessionTbl), 1);
+    summaryArray(session,:) = meanSessionTbl;
+end
+
+summaryTbl = array2table(summaryArray, "VariableNames", bestTbl.Properties.VariableNames);
+%%
+% date
+for session = 1:sessionNum
+    date = datetime(strcat(string(summaryTbl.Day(session)), "/",... 
+        string(summaryTbl.Month(session)), "/",... 
+        string(summaryTbl.Year(session))),... 
+        'InputFormat', 'dd/MM/uuuu', 'Format', 'dd/MM/uuuu');
+    dates(session,1) = date;
+end
+
+%%
+
+% graphs
+t = tiledlayout(2, 2);
+
+nexttile
+plot(summaryTbl.Session, summaryTbl.Lambda, 'Marker', 'x', 'Color', 'r', 'MarkerEdgeColor', 'k');
+title("Lambda by Session");
+nexttile
+plot(summaryTbl.Session, summaryTbl.Yellow, 'Marker', 'x', 'Color', 'b', 'MarkerEdgeColor', 'k');
+title("Yellow by Session");
+
+nexttile
+plot(dates, summaryTbl.Lambda, 'Marker', 'x', 'Color', 'r', 'MarkerEdgeColor', 'k');
+title("Lambda by Date");
+nexttile
+plot(dates, summaryTbl.Yellow, 'Marker', 'x', 'Color', 'b', 'MarkerEdgeColor', 'k');
+title("Yellow by Date");
