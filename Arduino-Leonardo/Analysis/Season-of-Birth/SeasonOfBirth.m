31091a5e560147949620bfe4b20f902903cc4963
Dana Turner <dana.turner@psy.ox.ac.uk>
2024-03-27 15:14:55 +0000

    radar graphs update

--- Arduino-Leonardo/Analysis/Season-of-Birth/SeasonOfBirth.m
+++ Arduino-Leonardo/Analysis/Season-of-Birth/SeasonOfBirth.m
@@ -1,6 +1,7 @@
 % clear up
 clc; clear; close all;
 
+%%
 % josh's data
 joshData = readtable("data-DJ.xlsx", "VariableNamingRule","preserve");
 
@@ -33,7 +34,6 @@ djData = [joshDataDemo joshDataResults];
 djData.RLM_RG = djData.RLM_Red ./ djData.RLM_Green;
 djData.HFP_RG = djData.HFP_RedValue ./ djData.HFP_GreenValue;
 
-%%
 monthMeansDJ = NaN(2, 12);
 monthNsDJ = NaN(2,12);
 for month = 1:12
@@ -44,11 +44,9 @@ for month = 1:12
 end
 
 %%
-
-% average rows
+% mitch's rlm data
 rlmVars = ["Red", "Green", "Yellow"];
 
-% mitch's rlm data
 mitchDataRLM = load("dataRLM-MT.mat","ParticipantMatchesRLM");
 mitchDataRLM = mitchDataRLM.ParticipantMatchesRLM;
 mitchDataRLM = mitchDataRLM(~contains(string(mitchDataRLM.ParticipantCode), "TEST"),:);
@@ -75,55 +73,134 @@ for ptpt = 1:length(ptptIDs)
 end
 
 mitchDataResultsRLM = array2table(mitchDataResultsRLM, "VariableNames", ["Month", rlmVars]);
-mitchDataResultsRLM.RG = mitchDataResultsRLM.Red ./ mitchDataResultsRLM.Green;
+mitchDataResultsRLM.RLM_RG = mitchDataResultsRLM.Red ./ mitchDataResultsRLM.Green;
 
-monthMeansMT = NaN(1,12);
-monthNsMT = NaN(1, 12);
+% mitches hfp data
+mitchDataHFP_files=dir(fullfile('dataHFP-MT','*.mat'));
+mitchDataResultsHFP = NaN(height(ptptIDs),1);
+for file = 1:length(mitchDataHFP_files)
+    fileName = mitchDataHFP_files(file).name;
+    currentFile = load(strcat(pwd, '\dataHFP-MT\', fileName));
+    currentFile = currentFile.hfpData;
+    currentFile = convertvars(currentFile, "trialID", 'string');
+    currentID = extractBefore(extractAfter(fileName, 'HFP_'), '_');
+    currentFile.trialID = repmat(currentID, [height(currentFile) 1]);
+    if file == 1, mitchDataHFP = currentFile;
+    else, mitchDataHFP = [mitchDataHFP; currentFile]; %#ok<AGROW>
+    end
+    
+    mitchDataResultsHFP(strcmp(currentID, ptptIDs)) = table2array(mean(currentFile(currentFile.trialNum > 1, "meanTestAmpSetting")));
+end
+mitchDataResultsHFP = array2table(mitchDataResultsHFP, "VariableNames", "HFP_RG");
+mitchDataResults = [mitchDataResultsRLM mitchDataResultsHFP];
+
+monthMeansMT = NaN(2,12);
+monthNsMT = NaN(2,12);
 for month = 1:12
-    monthDataMT = mitchDataResultsRLM(mitchDataResultsRLM.Month == month, "RG");
-    monthDataMT = monthDataMT(~isnan(table2array(monthDataMT)),:);
-    monthNsMT(month) = height(monthDataMT);
-    monthMeansMT(month) = table2array(mean(monthDataMT));
+    monthDataMT = mitchDataResults(mitchDataResults.Month == month, endsWith(mitchDataResults.Properties.VariableNames, "RG"));
+    monthNsMT(1,month) = height(monthDataMT(~isnan(table2array(monthDataMT(:,1))),:));
+    monthNsMT(2,month) = height(monthDataMT(~isnan(table2array(monthDataMT(:,2))),:));
+    monthMeansMT(:, month) = table2array(mean(monthDataMT, "omitmissing"))';
 end
 
+%%
+% Allie's data
+aData = load("data-A.mat");
+aData = aData.hfpTable;
+allieDataDemo = readtable("dataDemo-A.xlsx");
 
+ptptIDs = unique(string(aData.ptptID));
+
+allieResultsData = NaN(height(ptptIDs), 2);
+for ptpt = 1:height(aData)
+    idx = contains(string(allieDataDemo.LM100ID), ptptIDs(ptpt));
+    if sum(idx) == 1
+        dob = char(table2array(allieDataDemo(idx, "DOB")));
+        if ~isempty(dob), month = str2num(dob(4:5)); else, month = NaN; end
+        logRG = table2array(aData(ptpt, "meanTestAmp"));
+        allieResultsData(ptpt,:) = [month logRG];
+    end
+end
+
+idx = sum(isnan(allieResultsData),2) == width(allieResultsData);
+allieResultsData = allieResultsData(~idx,:);
+allieResultsData = array2table(allieResultsData, "VariableNames", ["Month", "amp"]);
+
+monthMeansA = NaN(1,12);
+monthNsA = NaN(1,12);
+for month = 1:12
+    monthDataA = allieResultsData(allieResultsData.Month == month, strcmp(allieResultsData.Properties.VariableNames, "amp"));
+    monthNsA(1,month) = height(monthDataA(~isnan(table2array(monthDataA(:,1))),:));
+    monthMeansA(:, month) = table2array(mean(monthDataA, "omitmissing"))';
+end
+
+%%
+% Dana's data
+vars = ["Month", "RLM_Red_1", "RLM_Green_1", "RLM_MixLight_1", "HFP_Leo_Red_1", "HFP_Leo_Green_1", "HFP_Uno_Red_1"];
+dataB = readtable("data-B.xlsx", 'Sheet','Matlab_Data');
+ptptIDs = unique(string(dataB.PPcode));
+danaData = array2table(NaN(length(ptptIDs), length(vars)), 'VariableNames', vars);
+for ptpt = 1:length(ptptIDs)
+    idx = strcmp(string(dataB.PPcode), ptptIDs(ptpt))...
+          & dataB.Study == 1.1;
+    currentData = dataB(idx,vars);
+    if ~isempty(currentData)
+        danaData(ptpt,:) = mean(currentData(2:end, :), 'omitmissing');
+    end
+end
+
+danaData.RLM_RG = danaData.RLM_Red_1 ./ danaData.RLM_Green_1;
+danaData.HFP_RG = danaData.HFP_Leo_Red_1 ./ danaData.HFP_Leo_Green_1;
+
+monthMeansB = NaN(4,12);
+monthNsB = NaN(4,12);
+for month = 1:12
+    monthDataB = danaData(danaData.Month == month, ["RLM_RG", "RLM_MixLight_1", "HFP_RG", "HFP_Uno_Red_1"]);
+    for i = 1:width(monthDataB)
+        monthNsB(i,month) = height(monthDataB(~isnan(table2array(monthDataB(:,i))),:));
+    end
+    monthMeansB(:, month) = table2array(mean(monthDataB, "omitmissing"))';
+end
+
+%%
+% radar graphs
 monthVars = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-monthTbl = array2table([monthMeansDJ; monthMeansMT], "RowNames", ["RLM-DJ", "HFP-DJ", "RLM-MT"], "VariableNames", monthVars);
+monthTbl = array2table([monthMeansDJ; monthMeansMT; monthMeansA; monthMeansB],... 
+    "RowNames", ["RLM-Leo-DJ", "HFP-Leo-DJ", "RLM-Leo-MT", "HFP-Uno-MT", "HFP-Uno-A", "RLM-Leo-B", "RLM-Anom-B", "HFP-Leo-B", "HFP-Uno-B"], "VariableNames", monthVars);
 
-monthLabsRLM = strings(1,12);
-monthLabsHFP = strings(1,12);
+monthLabs = strings(4,12);
 for month = 1:12
-    monthLabsRLM(month) = strcat(monthVars(month), " (nJosh = ", num2str(monthNsDJ(1,month)), ", nMitch = ", num2str(monthNsMT(month)), ")");
-    monthLabsHFP(month) = strcat(monthVars(month), " (nJosh = ", num2str(monthNsDJ(2,month)), ")");
+    monthLabs(1,month) = strcat(monthVars(month), " (nJosh = ", num2str(monthNsDJ(1,month)), ", nMitch = ", num2str(monthNsMT(1,month)), ", nDana = ", num2str(monthNsB(1,month)), ")");
+    monthLabs(2,month) = strcat(monthVars(month), " (nDana = ", num2str(monthNsB(2,month)), ")");
+    monthLabs(3,month) = strcat(monthVars(month), " (nJosh = ", num2str(monthNsDJ(2,month)), ", nDana = ", num2str(monthNsB(3,month)), ")");
+    monthLabs(4,month) = strcat(monthVars(month), " (nMitch = ", num2str(monthNsMT(2,month)), ", nAllie = ", num2str(monthNsA(month)), ", nDana = ", num2str(monthNsB(4,month)), ")");
 end
 
 figure(1)
-idx = contains(monthTbl.Properties.RowNames, "RLM");
-s = spider_plot(table2array(monthTbl(idx,:)), 'AxesLabels', cellstr(monthLabsRLM), 'AxesLimits',...
+idx = contains(monthTbl.Properties.RowNames, "RLM-Leo");
+s = spider_plot(table2array(monthTbl(idx,:)), 'AxesLabels', cellstr(monthLabs(1,:)), 'AxesLimits',...
     [repmat(table2array(min(monthTbl(idx,:), [], "all")), [1 12]); repmat(table2array(max(monthTbl(idx,:), [], "all")), [1 12])],...
-    'Color', [1 0 0; 0 1 0], 'FillOption', 'on', 'FillTransparency', .3);
-    title("RLM");
+    'Color', [1 0 0; 0 1 0; 1 0 1], 'FillOption', 'on', 'FillTransparency', .3);
+    title("RLM Leonardo");
 
 figure(2)
-idx = contains(monthTbl.Properties.RowNames, "HFP");
-s = spider_plot(table2array(monthTbl(idx,:)), 'AxesLabels', cellstr(monthLabsHFP), 'AxesLimits',...
+idx = contains(monthTbl.Properties.RowNames, "RLM-Anom");
+s = spider_plot(table2array(monthTbl(idx,:)), 'AxesLabels', cellstr(monthLabs(2,:)), 'AxesLimits',...
     [repmat(table2array(min(monthTbl(idx,:), [], "all")), [1 12]); repmat(table2array(max(monthTbl(idx,:), [], "all")), [1 12])],...
-    'Color', [1 0 0], 'FillOption', 'on', 'FillTransparency', .3);
-    title("HFP");
+    'Color', [1 0 1], 'FillOption', 'on', 'FillTransparency', .3);
+    title("RLM Anomaloscope");
 
-% % mitches hfp data
-% mitchDataHFP_files=dir(fullfile('dataHFP-MT','*.mat'));
-% for file = 1:length(mitchDataHFP_files)
-%     fileName = mitchDataHFP_files(file).name;
-%     currentFile = load(strcat(pwd, '\dataHFP-MT\', fileName));
-%     currentFile = currentFile.hfpData;
-%     currentFile = convertvars(currentFile, "trialID", 'string');
-%     currentFile.trialID = extractBefore(currentFile.trialID, "_");
-%     if file == 1, mitchDataHFP = currentFile;
-%     else, mitchDataHFP = [mitchDataHFP; currentFile]; %#ok<AGROW>
-%     end
-% end
-% 
-% mitchDataHFP.trialID(6:10) = "MAB";
+figure(3)
+idx = contains(monthTbl.Properties.RowNames, "HFP-Leo");
+s = spider_plot(table2array(monthTbl(idx,:)), 'AxesLabels', cellstr(monthLabs(3,:)), 'AxesLimits',...
+    [repmat(table2array(min(monthTbl(idx,:), [], "all")), [1 12]); repmat(table2array(max(monthTbl(idx,:), [], "all")), [1 12])],...
+    'Color', [1 0 0; 1 0 1], 'FillOption', 'on', 'FillTransparency', .3);
+    title("HFP Leonardo");
 
+figure(4)
+idx = contains(monthTbl.Properties.RowNames, "HFP-Uno");
+s = spider_plot(table2array(monthTbl(idx,:)), 'AxesLabels', cellstr(monthLabs(4,:)), 'AxesLimits',...
+    [repmat(table2array(min(monthTbl(idx,:), [], "all")), [1 12]); repmat(table2array(max(monthTbl(idx,:), [], "all")), [1 12])],...
+    'Color', [0 1 0; 0 0 1; 1 0 1], 'FillOption', 'on', 'FillTransparency', .3);
+    title("HFP Uno");
